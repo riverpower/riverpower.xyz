871a1f5334bd69e2efd6cff1662063ed9bfc9b30
Christopher Goulet <chris@uproot.tech>
2021-05-01 08:55:36 -0400

    update radio url

--- web/src/pages/Radio/Radio.js
+++ web/src/pages/Radio/Radio.js
@@ -8,7 +8,7 @@ class Radio extends React.Component {
       <Container className='radio-page'>
         <Row className='align-items-center' style={{textAlign: 'center', paddingTop: 24 }}>
           <Col xs='12' sm='12' md='12' lg='12'>
-            <iframe src="https://radio.uprootx.com/public/riverpower_radio/embed?theme=light" title="radio" frameborder="0" allowtransparency="true" styles="width: 100%; min-height: 150px; border: 0;"></iframe>            
+            <iframe src="https://radio.riverpower.xyz/public/riverpower_radio/embed?theme=light" title="radio" frameborder="0" allowtransparency="true" styles="width: 100%; min-height: 150px; border: 0;"></iframe>            
           </Col>                    
         </Row>
       </Container>  
